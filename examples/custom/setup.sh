01c045f7d013844763d64f44754b28b28d4dd3d0
George Ohashi <george@neuralmagic.com>
2024-03-14 19:07:38 +0000

    simple setup

---
+++ examples/custom/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+## BENCHMARK SCRIPT. LAUNCH server and client
+
+
+python3 -V
+python3 -m venv venv
+source venv/bin/activate
+
+pip3 install -e . 
+pip3 install aiohttp
+
+# start a server on process 1
+<<COMMENT
+python3 -m vllm.entrypoints.openai.api_server \
+    --model mistralai/Mistral-7B-v0.1 \
+    --max-model-len 2048 \
+    --disable-log-requests
+COMMENT
+
+# make requests to the server (note this is the vllm benchmark,
+# not the nm-vllm benchmark that Varun made
+
+<<COMMENT
+wget https://huggingface.co/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered/resolve/main/ShareGPT_V3_unfiltered_cleaned_split.json
+
+python3 benchmarks/benchmark_serving.py \
+    --model mistralai/Mistral-7B-v0.1 \
+    --tokenizer mistralai/Mistral-7B-v0.1 \
+    --endpoint /v1/completions \
+    --dataset ShareGPT_V3_unfiltered_cleaned_split.json \
+    --request-rate 3.0 \
+    --backend openai
+COMMENT
+
